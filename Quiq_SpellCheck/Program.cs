375e5ec6d7355005ac106a3b9f5ac7740312be32
Aaron McCarthy <amccarthy9904@gmail.com>
2018-03-11 16:25:37 -0600

    initial setup, load words into list, might change datam struct

---
+++ Quiq_SpellCheck/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiq_SpellCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader stream = new StreamReader("test.txt"))
+            {
+                List<string> dic = new List<string>();
+                while (!stream.EndOfStream)
+                {
+                    dic.Add(stream.ReadLine());
+                }
+
+                mainMenu(new SpellCheck(dic));
+            }
+        }
+
+        private static void mainMenu(SpellCheck spellCheck)
+        {
+            bool exit = false;
+            while (!exit)
+            {
+
+            }
+        }
+    }
+}
---
+++ Quiq_SpellCheck/SpellCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiq_SpellCheck
+{
+    class SpellCheck
+    {
+        private List<string> dic;
+
+        public SpellCheck(List<string> dic)
+        {
+            this.dic = dic;
+        }
+    }
+}
