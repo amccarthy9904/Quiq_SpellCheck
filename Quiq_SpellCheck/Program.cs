21879052935a5d30910d146cf01fbde891a5f3aa
Aaron McCarthy <amccarthy9904@gmail.com>
2018-03-19 20:00:16 -0600

    updated README

--- Quiq_SpellCheck/Program.cs
+++ Quiq_SpellCheck/Program.cs
@@ -1,62 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Windows.Forms;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Collections;
 
 namespace Quiq_SpellCheck
 {
     class Program
     {
+        /// <summary>
+        /// Setup dictionary, create SpellCheck object abd call start()
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
         static void Main(string[] args)
         {
-            //List<string> dic = new List<string>();
             Hashtable dic = new Hashtable();
-            using (StreamReader stream = new StreamReader("words.txt"))
+            Hashtable dicCommon = new Hashtable();
+
+            //create small dictionary of common words
+            using (StreamReader commonStream = new StreamReader("wordsCommon.txt"))
             {
-                while (!stream.EndOfStream)
+                while (!commonStream.EndOfStream)
                 {
-                    dic.Add(stream.ReadLine(), true);
+                    dicCommon.Add(commonStream.ReadLine(), true);
                 }
             }
-            mainMenu(new SpellCheck(dic), args[0]);
-        }
 
-        private static void mainMenu(SpellCheck spellCheck, string filePath)
-        {
-            //Console.WriteLine("~~~~WELCOME TO SPELL CHECK~~~~");
-            //Console.WriteLine("Enter a word or press ESC to exit");
-            //while (Console.ReadKey(false).Key != ConsoleKey.Escape)
-
+            //make lookup dictionary
             using (StreamReader stream = new StreamReader("words.txt"))
             {
-                int count = 0;
                 while (!stream.EndOfStream)
                 {
-                    count++;
-                    if (count % 10000 == 0) { Console.WriteLine(count); }
+                    //add every line in file to dictionary
+                    dic.Add(stream.ReadLine(), false);
+                }
+            }
+
+            //label common words with true value
+            foreach (string commonWord in dicCommon.Keys)
+            {
+                if (dic.Contains(commonWord))
+                {
+                    dic[commonWord] = true;
+                }
+            }
+
+            //start reading in words and checking their spelling
+            start(new SpellCheck(dic), args[0]);
+        }
+        
+        /// <summary>
+        /// Reads in words from file path given in command line and checks each of them
+        /// </summary>
+        /// <param name="spellCheck">Spell check object uset to check the words</param>
+        /// <param name="filePath">Path to input file</param>
+        private static void start(SpellCheck spellCheck, string filePath)
+        {
+            //open file to read in words
+            using (StreamReader stream = new StreamReader(filePath))//TODO change this to filepath
+            {
+                while (!stream.EndOfStream)
+                {
                     var word = stream.ReadLine();
+
+                    //if word is in the dictionary
                     if (spellCheck.lookup(word))
                     {
-                        //Console.Write("C");
+                        Console.WriteLine("CORRECT:\t" + word);
                     }
-                    else if (false)
-                    {
-                        //suggestion here
-                    }
+
+                    //if the word is not in the dictionary, attempt to find suggestion
+                    else if (spellCheck.suggest(word)) { }
+
+                    //if no suggestion found print incorrect
                     else
                     {
                         Console.WriteLine("INCORRECT:\t" + word);
-                        Console.WriteLine("on line " + count);
                     }
-
                 }
             }
-            Console.WriteLine();
-            Console.WriteLine("~~~~~~~~~~END OF TEST~~~~~~~~");
+            //wait for user to press esc key to exit
             Console.WriteLine("Press ESC to exit");
             while (Console.ReadKey(false).Key != ConsoleKey.Escape) { }
         }
