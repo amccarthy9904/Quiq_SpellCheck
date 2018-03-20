1de96a5253a1283aa31e1bd21de20d6a1d8c367d
Aaron McCarthy <amccarthy9904@gmail.com>
2018-03-19 20:01:12 -0600

    Revert "updated README"
    This reverts commit 21879052935a5d30910d146cf01fbde891a5f3aa.

--- Quiq_SpellCheck/SpellCheck.cs
+++ Quiq_SpellCheck/SpellCheck.cs
@@ -2,129 +2,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Quiq_SpellCheck
 {
     class SpellCheck
     {
-        /// <summary>
-        /// Dictionary used to lookup words
-        /// </summary>
         private Hashtable dic;
 
-        /// <summary>
-        /// Initializes and instace of the SpellCheck class
-        /// </summary>
-        /// <param name="dic">Dictionary that holds all words needed and whether or not they are common words</param>
         public SpellCheck(Hashtable dic)
         {
             this.dic = dic;
         }
 
-        /// <summary>
-        /// Looks up a word in the dictionary
-        /// </summary>
-        /// <param name="query">Word to find in the dictionary</param>
-        /// <returns>True if query is contained in the dictioary, false otherwise</returns>
         public bool lookup(string query)
         {
-            return this.dic.ContainsKey(query);
+            if (this.dic.ContainsKey(query))
+            {
+                return true;
+            }
+            else if (false)
+            {
+                //suggest word
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        /// <summary>
-        /// Suggests a possible spelling suggestion based on the misspelled word
-        /// </summary>
-        /// <param name="word">Incorrectly spelled word</param>
-        /// <returns>True if a suggestion was found, False otherwise</returns>
-        internal bool suggest(string word)
-        {
-            //holds all possible suggestions
-            var suggestionList = new List<Tuple<int, string>>();
 
-            //holds all words in dictionary
-            var suggestions = dic.Keys;
-            foreach (object suggestion in suggestions)
-            {
-                //find the Levenshtein distance between the suggestion and the original word
-                int distance = levenshtein(suggestion.ToString(), word);
+        //private List<string> dic;
 
-                //only add to list of suggestions if suggestion is less than 4 transformations away from original word
-                if (distance < 4)
-                {
-                    suggestionList.Add(Tuple.Create(distance, suggestion.ToString()));
-                }
-            }
+        //public SpellCheck(List<string> dic)
+        //{
+        //    this.dic = dic;
+        //}
 
-            //if any suggestions were found
-            if (suggestionList.Count != 0)
-            {
-                //find the min distance for all suggestions
-                int min = suggestionList.Min().Item1;
 
-                //attempt to find a suggestion with the minimum distance that is also a common word
-                var commonSugg = suggestionList.Find(common => common.Item1 == min && (bool)this.dic[common.Item2]);
-
-                //if a common suggestion was found
-                if (commonSugg != null)
-                {
-                    Console.WriteLine("SUGGESTION for " + word + " -> " + commonSugg.Item2);
-                    return true;
-                }
-                else
-                {
-                    Console.WriteLine("SUGGESTION for " + word + " -> " + suggestionList.Min().Item2);
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        private int levenshtein(string suggestion, string input)
-        {
-            //store the dimensions of the matrix
-            var xLen = suggestion.Length + 1;
-            var yLen = input.Length + 1;
-            //create char arrays from input strings
-            var suggArray = suggestion.ToCharArray();
-            var inArray = input.ToCharArray();
-            
-            //create Levenshtein Distance matrix
-            int[,] matrix = new int[xLen, yLen];
-
-            //init first row and first column to distances from empty strings
-            for (int iter = 0; iter < xLen; iter++)
-            {
-                matrix[iter, 0] = iter;
-            }
-            for (int iter = 0; iter < yLen; iter++)
-            {
-                matrix[0, iter] = iter;
-            }
-
-            //fill matrix with distance values
-            //dont change first coluymn or first row
-            for (int yIter = 1; yIter < yLen; yIter++)
-            {
-                for (int xIter = 1; xIter < xLen; xIter++)
-                {
-                    int subCost = 1;
-
-                    //if the two chars are the same there is no substitution cost
-                    if (suggArray[xIter - 1] == inArray[yIter - 1])
-                    {
-                        subCost = 0;
-                    }
-                    
-                    //take 3 values from space above, the space to the left and the space above and to the left 
-                    var nextVal = new int[] { matrix[xIter - 1, yIter] + 1, matrix[xIter, yIter - 1] + 1, matrix[xIter - 1, yIter - 1] + subCost };
-                    
-                    //insert the minimum of those values into the current spot in the matrix
-                    matrix[xIter,yIter] =  nextVal.Min();
-                }
-            }
-            //return the bottom right value in the matrix - the Levenshtein distance
-            return matrix[xLen - 1, yLen - 1];
-        }
+        //public bool lookup(string query)
+        //{
+        //    if (dic.BinarySearch(query, StringComparer.CurrentCultureIgnoreCase) >= 0)
+        //    {
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        return false;
+        //    }
+        //}
     }
 }
--- README.md
+++ README.md
@@ -1,28 +1,2 @@
 # Quiq_SpellCheck
 Spell Check coding Exercise for Quick
-
-In order to run the solution words.txt and wordsCommon.txt must be in the working directory and a file path to the input file must be specified as a command line argument. 
-
-SOLUTION HIGHLIGHTS
-	My spell checker uses a HashTable to store the dictionary for quick lookups. The HashTable consists of <string, bool> Key
-	/Value pairs. The string is the word and the bool indicates if the word is common. To decide what words are common I 
-	found a smaller dictionary of ~100,000 words online and stored these words in wordsCommon.txt. This smaller subset of 
-	words represents more commonly used words in the English Vernacular. I made a HashTable for this file as well and 
-	changed all values for all common keys between the two Hashtables to true.	This boolean is used when suggesting words 
-	as words in the HashTable with a true value will be selected before words with a false vale.
-
-	My solution uses Levenshtein Distance to determine which word to suggest when it encounters an incorrectly spelled word. 
-	Levenshtein Distance is a metric for determining the similarity of strings. It is calculated by finding the number of 
-	transformations (insertions, deletions, or substitutions) required to change one string to another. My solution finds 
-	all words in the dictionary whose Levenshtein Distance from the misspelled word is less than 4. From this set of 
-	suggestions my solution first searches for the suggestion with the lowest Levenshtein Distance that has also been 
-	labeled as a common word. If no common word is found then it suggests the first word in the suggestion set that has the 
-	minimum Levenshtein Distance. If there are no suggestions with a Levenshtein Distance of less than 4 then my solution 
-	classifies the word as incorrect.
-
-ASSUMPTIONS
-	For this solution I assumed that the word in the dictionary with the lowest Levenshtein Distance is the best to suggest. 
-	However often there are multiple words that are tied for the lowest Levenshtein Distance and I had to classify every 
-	word as common or not in order to provide better suggestions.
-
-If you have any questions send me an email at amccarthy9904@gmail.com
